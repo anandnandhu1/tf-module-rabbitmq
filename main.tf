663d8a29f7ab4eaf338df8419e30f9a94821bd4a
anand nandhu <anandnandhu52@gmail.com>
2023-05-19 11:31:12 +0530

    terraform

--- main.tf
+++ main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "rabbitmq" {
     cidr_blocks = var.allow_cidr
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -32,29 +40,52 @@ resource "aws_security_group" "rabbitmq" {
 //  data           = ""
 //}
 
+# we moved from service to ec2 node for rabbitmq
 
-resource "aws_mq_broker" "rabbitmq" {
-  broker_name        = "${var.env}-rabbitmq"
-  deployment_mode    = var.deployment_mode
-  engine_type        = var.engine_type
-  engine_version     = var.engine_version
-  host_instance_type = var.host_instance_type
-  security_groups    = [aws_security_group.rabbitmq.id]
-  subnet_ids         = var.deployment_mode == "SINGLE_INSTANCE" ? [var.subnet_ids[0]] : var.subnet_ids
+//resource "aws_mq_broker" "rabbitmq" {
+//  broker_name        = "${var.env}-rabbitmq"
+//  deployment_mode    = var.deployment_mode
+//  engine_type        = var.engine_type
+//  engine_version     = var.engine_version
+//  host_instance_type = var.host_instance_type
+//  security_groups    = [aws_security_group.rabbitmq.id]
+//  subnet_ids         = var.deployment_mode == "SINGLE_INSTANCE" ? [var.subnet_ids[0]] : var.subnet_ids
 
   //  configuration {
   //    id       = aws_mq_configuration.rabbitmq.id
   //    revision = aws_mq_configuration.rabbitmq.latest_revision
   //  }
 
-  encryption_options {
-    use_aws_owned_key = false
-    kms_key_id        = data.aws_kms_key.key.arn
-  }
+ // encryption_options {
+ //   use_aws_owned_key = false
+ //   kms_key_id        = data.aws_kms_key.key.arn
+ // }
 
-  user {
-    username = data.aws_ssm_parameter.USER.value
-    password = data.aws_ssm_parameter.PASS.value
-  }
+//  user {
+//    username = data.aws_ssm_parameter.USER.value
+//    password = data.aws_ssm_parameter.PASS.value
+//  }
 
+//}
+
+resource "aws_spot_instance_request" "rabbitmq" {
+  ami                    = data.aws_ami.centos8.image_id
+  instance_type          = "t3.small"
+  subnet_id              = var.subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.rabbitmq.id]
+  wait_for_fulfillment   = true
+  user_data              = base64encode(templatefile("${path.module}/user-data.sh", { component = "rabbitmq", env = var.env }))
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-rabbitmq" }
+  )
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z09288323SGMRZAARID6Z"
+  name    = "rabbitmq-${var.env}.anandnandhu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_spot_instance_request.rabbitmq.private_ip]
 }
